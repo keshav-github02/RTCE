a7d71481c73e32e2d20e38389f3204df3a4282ed
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2026-04-06 23:01:44 +0530

    setting up server

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,8 +1,62 @@
 import "./App.css"
 import {Editor} from "@monaco-editor/react"
 import { MonacoBinding } from "y-monaco"
+import * as Y from "yjs"
+import { useRef,useMemo,useState } from "react"
+import {SocketIOProvider} from "y-socket.io"
+
+
 
 function App() {
+
+  const editorRef = useRef(null);
+ const [ username, setUsername ] = useState(() => {
+    return new URLSearchParams(window.location.search).get("username") || ""
+  })
+
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+  
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc,{autoConnect: true})
+    const monacoBinding = new MonacoBinding(yText, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+
+   
+  }
+
+    const handleJoin = (e) => {
+    e.preventDefault()
+    setUsername(e.target.username.value)
+     window.history.pushState({}, "", "?username=" + e.target.username.value)
+
+  }
+
+
+   if (!username) {
+    return (
+      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center" >
+        <form
+          onSubmit={handleJoin}
+          className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Enter your username"
+            className="p-2 rounded-lg bg-gray-800 text-white"
+            name="username"
+          />
+          <button
+            className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+          >
+            Join
+          </button>
+        </form>
+      </main>
+    )
+  }
+
 
   return (
     <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
@@ -17,6 +71,7 @@ function App() {
           defaultLanguage="javascript"
           defaultValue="// some comment"
           theme="vs-dark"
+          onMount={handleMount}
 
         />
         </section>
